a96b8494ad6302de4a156cbbf87ce391135d699b
esteri1 <eadeyemi24@gmail.com>
2022-06-30 14:56:33 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+/**
+ * main - a function that prints out a message
+ * Return: returns a value of 1 if when it runs successfully
+ */
+#include <stdio.h>
+#include <unistd.h>
+int main (void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
+}
