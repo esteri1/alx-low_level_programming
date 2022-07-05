23a910e0d8775bc04ef2464e42e2999535e9e091
esteri1 <eadeyemi24@gmail.com>
2022-07-05 10:14:45 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
-#include <unistd.h>
+#include "libioP.h"
+#include "stdio.h"
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+#undef putchar
+
+int
+putchar (int c)
 {
-	return (write(1, &c, 1));
+  int result;
+  _IO_acquire_lock (_IO_stdout);
+  result = _IO_putc_unlocked (c, _IO_stdout);
+  _IO_release_lock (_IO_stdout);
+  return result;
 }
