8f46db51b3349229167aa345f346f70e35739a97
esteri1 <eadeyemi24@gmail.com>
2022-07-07 13:01:04 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
-*
-*Return: void
-*/
+ * print_numbers - print 0123456789
+ *
+ * Return: void
+ */
 
 void print_numbers(void)
 {
@@ -12,7 +12,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	void_putchar(c);
+	_putchar(c);
 	}
 	_putchar('\n');
 }
