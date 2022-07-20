3ac5fc67175341df5cfbf68d65fdbaa961c83b27
esteri1 <eadeyemi24@gmail.com>
2022-07-20 06:09:59 -0700

    Task 0

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This is my 0x08-recursion in alx-low_level_programming
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#include <stdio.h>
+#endif /*MAIN_H*/
